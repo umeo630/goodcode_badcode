c523a81964c445b02df5e9a1d21fccb73042d265
Hikaru-Umezawa <hikaru.19970630@gmail.com>
2023-06-10 23:08:39 +0900

    3.2.1 コンストラクタ

---
+++ src/Money.java
@@ -0,0 +1,12 @@
+public class Money {
+    int amount;
+    Currency currency;
+
+    Money(int amount, Currency currency) {
+        // ガード節で不正値の混入を防ぐ
+        if (amount < 0) throw new IllegalArgumentException("amount must be positive");
+        if (currency == null) throw new IllegalArgumentException("currency must be specified");
+        this.amount = amount;
+        this.currency = currency;
+    }
+}
