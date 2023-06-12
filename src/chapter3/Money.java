1b19a15b2890e667426ceb2e8043d34f82299f24
Hikaru-Umezawa <hikaru.19970630@gmail.com>
2023-06-12 21:41:32 +0900

    3.2.5 メソッド引数やローカル変数も不変にする

--- src/chapter3/Money.java
+++ src/chapter3/Money.java
@@ -4,7 +4,7 @@ public class Money {
     final int amount;
     final Currency currency;
 
-    Money(int amount, Currency currency) {
+    Money(final int amount, final Currency currency) {
         // ガード節で不正値の混入を防ぐ
         if (amount < 0) throw new IllegalArgumentException("amount must be positive");
         if (currency == null) throw new IllegalArgumentException("currency must be specified");
@@ -12,8 +12,8 @@ public class Money {
         this.currency = currency;
     }
 
-    Money add(int other) {
-        int added = amount + other;
+    Money add(final int other) {
+        final int added = amount + other;
         return new Money(added, currency);
     }
 }
