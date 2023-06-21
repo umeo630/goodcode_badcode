980e6c8ad9d654620f24a68db1f32b180adaf4b8
Hikaru-Umezawa <hikaru.19970630@gmail.com>
2023-06-21 21:48:49 +0900

    5.2.1 privateコンストラクタ + ファクトリメソッド ・GiftPointクラスの改善

--- src/chapter5/GiftPoint.java
+++ src/chapter5/GiftPoint.java
@@ -1,12 +1,30 @@
 class GiftPoint {
     private static final int MIN_POINT = 0;
+    private static final int STANDARD_MEMBER_SHIP_POINT = 100;
+    private static final int PREMIUM_MEMBER_SHIP_POINT = 300;
     final int value;
 
-    GiftPoint(final int point) {
+    // 外部からはインスタンス生成できない
+    // クラス内部でのみインスタンス生成できる
+    private GiftPoint(final int point) {
         if (point < MIN_POINT) {
             throw new IllegalArgumentException("ポイントが0以上ではありません。");
         }
         value = point;
+    }
+
+    /**
+     * @return 標準会員向け入会ギフトポイント
+     */
+    static GiftPoint forStandardMemberShip() {
+        return new GiftPoint(STANDARD_MEMBER_SHIP_POINT);
+    }
+
+    /**
+     * @return プレミアム会員向け入会ギフトポイント
+     */
+    static GiftPoint forPremiumMemberShip() {
+        return new GiftPoint(PREMIUM_MEMBER_SHIP_POINT);
     }
 
     /**
--- src/chapter5/main.java
+++ src/chapter5/main.java
@@ -3,8 +3,8 @@ public class main {
      * 様々な用途で使用されてしまう
      */
     // 標準会員の入会ポイント
-    GiftPoint standardMemberShipPoint = new GiftPoint(100);
+    GiftPoint standardMemberShipPoint = GiftPoint.forStandardMemberShip();
 
     // プレミアム会員の入会ポインｔ
-    GiftPoint premiumMemberShipPoint = new GiftPoint(300);
+    GiftPoint premiumMemberShipPoint = GiftPoint.forPremiumMemberShip();
 }
